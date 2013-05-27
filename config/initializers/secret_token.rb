a0d1c979c5772dff1c6add80244d9ca82f16aaf4
mwinkler <mhwinkler@gmail.com>
2013-05-27 17:57:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FishTally::Application.config.secret_token = '65567a441ea3204cb92830b58724fdaf9d0bbd341aabb2a5eaa62b2966c264b4bdbae6d3f27c18d47d31a780699f9165fdd970cc5b39ad23d16e26441baaa6a2'
