b0317dc89ee90ed28ecc04d673b65cba28f932a9
mwinkler <mhwinkler@gmail.com>
2013-05-30 17:18:45 -0500

    Added sinatra-bootstrap

--- app.rb
+++ app.rb
@@ -7,11 +7,20 @@ require "sinatra"
 
 # The app
 class FishTally < Sinatra::Base
-  get "/" do
-    "Hello, World!"
+  configure do
+    set :public_folder, Proc.new { File.join(root, "static") }
+  end
+
+  get '/' do
+    erb :about
   end
   
-  get "/about" do
-    "About FishTally"
+  get '/about' do
+    erb :about
   end
+  
+  get '/contact' do
+    erb :contact
+  end
+
 end
